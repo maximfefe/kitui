83b52439193ccd781e01dcc1b6183ac01c5f80ef
Maxim <maxim.fedelique@gmail.com>
2023-05-09 17:42:45 +0200

    grid + preview

--- vue-project/src/views/PreviewView.vue
+++ vue-project/src/views/PreviewView.vue
@@ -3,6 +3,11 @@ import PreviewContainer from '@/components/PreviewContainer.vue'
 import ButtonComponent from '@/components/Component/ButtonComponent.vue'
 import LinkComponent from '@/components/Component/LinkComponent.vue'
 import CardComponent from '@/components/Component/CardComponent.vue'
+import GridComponent from '@/components/Component/GridComponent.vue'
+import PreviewTitle from '@/components/PreviewTitle.vue'
+
+import {useKituiStore} from '@/stores/kitui'
+import {mapStores} from 'pinia'
 
 export default {
   components:{
@@ -10,6 +15,17 @@ export default {
     ButtonComponent,
     LinkComponent,
     CardComponent,
+    GridComponent,
+    PreviewTitle
+  },
+  data() {
+    const kituiStore = useKituiStore()
+    return {
+      nbColumn: kituiStore.kitui.layout.grid.nbColumn
+    }
+  },
+  computed: {
+    ...mapStores(useKituiStore),
   }
 }
 </script>
@@ -18,30 +34,31 @@ export default {
     <div class="preview m-2">
       <PreviewContainer class="">
           <template #content>
-              <TitleComponent reductSize="1">
-                <h1>Rendu de votre configuration</h1>
-              </TitleComponent>
-              <TitleComponent reductSize="0.9">
-                <h2>Bouton et lien</h2>
-              </TitleComponent>
+            <TitleComponent reductSize="1">
+              <h1>Rendu de votre configuration</h1>
+            </TitleComponent>
+            <TitleComponent reductSize="0.9">
+              <h2>Bouton et lien</h2>
+            </TitleComponent>
+            <GridComponent>
               <ButtonComponent/>
               <LinkComponent :link="'#'" :content="'Lien cliquez ici'"/>
-              <TitleComponent reductSize="0.9">
-                <h2>Les cartes</h2>
-              </TitleComponent>
-              <div class="flex">
-                <CardComponent>
-                  <template #content>
-                    <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
-                    <ButtonComponent/>
-                  </template>
-                </CardComponent>
-                <CardComponent>
-                  <template #content>
-                    <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
-                    <ButtonComponent/>
-                  </template>
-                </CardComponent>
+            </GridComponent>
+            <TitleComponent reductSize="0.9">
+              <h2>La carte</h2>
+            </TitleComponent>
+            <CardComponent>
+              <template #content>
+                <TextComponent>
+                <LinkComponent :link="'#'" :content="'Lorem ipsum dolor sit'"/>, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?
+                </TextComponent>
+              </template>
+            </CardComponent>
+            <TitleComponent reductSize="0.9">
+              <h2>La grille</h2>
+            </TitleComponent>
+            <GridComponent>
+              <div v-for="index in parseInt(nbColumn)" :key="index">
                 <CardComponent>
                   <template #content>
                     <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
@@ -49,6 +66,33 @@ export default {
                   </template>
                 </CardComponent>
               </div>
+            </GridComponent>
+            <TitleComponent reductSize="1">
+              <h1>Titre et paragraphe</h1>
+            </TitleComponent>
+            <div class="">
+              <TitleComponent reductSize="0.9">
+                <h2>Titre de taille 2</h2>
+              </TitleComponent>
+              <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
+              <TitleComponent reductSize="0.8">
+                <h3>Titre de taille 3</h3>
+              </TitleComponent>
+              <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
+              <TitleComponent reductSize="0.7">
+                <h4>Titre de taille 4</h4>
+              </TitleComponent>
+              <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
+              <TitleComponent reductSize="0.6">
+                <h5>Titre de taille 5</h5>
+              </TitleComponent>
+              <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
+              <TitleComponent reductSize="0.5">
+                <h6>Titre de taille 6</h6>
+              </TitleComponent>
+              <TextComponent>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis eos consequuntur ratione a, esse laudantium veritatis. Qui rerum sunt blanditiis tenetur facere fuga voluptates nam distinctio, inventore corporis voluptatum. Enim?</TextComponent>
+
+            </div>
           </template>
       </PreviewContainer>
     </div>
@@ -56,11 +100,5 @@ export default {
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
